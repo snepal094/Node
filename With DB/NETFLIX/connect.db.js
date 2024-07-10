15cbc335d0de87d1c1196eab5a54c46fcd26ffe2
snepal094 <nepalsuyasha@gmail.com>
2024-07-10 15:15:18 +0545

    with db

--- With DB/NETFLIX/connect.db.js
+++ With DB/NETFLIX/connect.db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dbUserName = "kec";
+const dbUserName = "nepalsuyasha";
 const dbPassword = encodeURIComponent("kantipur");
 const dbHost = "cluster0.dvobivh.mongodb.net";
 const dbName = "kec-b4-netflix";
@@ -8,8 +8,7 @@ const dbName = "kec-b4-netflix";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+s/rv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
-      // "mongodb+srv://nepalsuyasha:kantipur@cluster0.dvobivh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      `mongodb+srv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("Connection established.");
   } catch (error) {
